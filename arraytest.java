0237a1634d772e65447750f9802644ad8f4aa08c
rreddy36 <rajashekhar.reddy@punchh.com>
2022-12-26 11:49:18 +0530

    Intial commit

---
+++ arraytest.java
@@ -0,0 +1,18 @@
+
+public class arraytest {
+	
+	public static void main(String args[]) {
+		
+		int a[]= {1,8,2,4,9};
+		//int s1=a.length;
+		for(int i:a) {
+				
+			System.out.println(a[i]);					
+			}
+			
+			
+		}
+		
+	}
+
+	
